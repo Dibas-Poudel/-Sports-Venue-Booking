378c7883be9e30775d41fda851cbc7f0382631a3
Dibas45 <Dibaspoudel45@gmail.com>
2025-06-08 14:31:39 +0545

    fix url

--- src/store/slice/user.js
+++ src/store/slice/user.js
@@ -3,7 +3,7 @@ import axios from "axios";
 import { toast } from "react-toastify";
 
 // API base URL
-const BASE_URL = "https://sportvenuebackend.onrender.com"
+const BASE_URL = "https://sportvenuebackend.onrender.com/api/v1"
 // const BASE_URL="http://localhost:3000/api/v1"   This is completely wroking on my local server
 
 // Thunk: User login
